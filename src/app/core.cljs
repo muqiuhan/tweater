90b63b2e8efa0e0acf5b784658563df01656e464
Muqiu Han <muqiu-han@outlook.com>
2023-07-07 14:57:43 +0800

    Add style

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -10,9 +10,32 @@
 
 (defnc root-view []
   (d/div {:class "container"}
-         (d/button {:id "submit-file"
-                    :text "open file"
-                    :on-click open-file})))
+         (d/div {:class "search-box"}
+                (d/i {:class "fa-solid fa-location-dot"})
+                (d/input {:type "text"
+                          :placeholder "Enter your location"})
+                (d/button {:class "fa-solid fa-magnifying-glass"}))
+
+         (d/div {:class "location-not-found"}
+                (d/img {:src "images/location-not-found.png"})
+                (d/p "Oops! Invalid location!"))
+
+         (d/div {:class "weather-box"}
+                (d/img {:src ""})
+                (d/p {:class "temperature"})
+                (d/p {:class "description"}))
+
+         (d/div {:class "weather-details"}
+                (d/div {:class "humidity"}
+                       (d/i {:class "fa-solid fa-water"})
+                       (d/div {:class "text"}
+                              (d/span)
+                              (d/p "Humidity")))
+                (d/div {:class "wind"}
+                       (d/i {:class "fa-solid fa-wind"})
+                       (d/div {:class "text"}
+                              (d/span)
+                              (d/p "Wind Speed"))))))
 
 (defonce root (rdc/createRoot (js/document.getElementById "root")))
 
