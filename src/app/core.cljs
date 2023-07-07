c9530e2b541a44a7f130ebadd520533b19b972a7
Muqiu Han <muqiu-han@outlook.com>
2023-07-07 17:46:16 +0800

    Add implementation

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -3,10 +3,79 @@
             [helix.core :refer [$]]
             [helix.dom :as d]
             [helix.hooks :as hooks]
-            ["react-dom/client" :as rdc]
-            ["@tauri-apps/api/tauri" :as tauri]))
+            ["react-dom/client" :as rdc]))
 
-(defn open-file [])
+(defn request-api [city]
+  (let [api-key "a98bb0392b4fb2a0796be798ae4a38ac"]
+    (when [(seq city)]
+      (->
+       (js/fetch
+        (str "https://api.openweathermap.org/data/2.5/weather?q="
+             city
+             "&units=metric"
+             "&appid="
+             api-key))
+       (.then (fn [response] (.json response)))))))
+
+(defn error-location-not-found []
+  (let [container (js/document.querySelector ".container")
+        weather-box (js/document.querySelector ".weather-box")
+        weather-details (js/document.querySelector ".weather-details")
+        location-not-found (js/document.querySelector ".location-not-found")]
+    (.setProperty (. container -style) "height" "400px")
+    (.setProperty (. weather-box -style) "display" "none")
+    (.setProperty (. weather-details -style) "display" "none")
+    (.setProperty (. location-not-found -style) "display" "block")
+    (.add (. location-not-found -classList) "fadeIn")))
+
+(defn set-weather-info [json]
+  (let [container (js/document.querySelector ".container")
+        image (js/document.querySelector ".weather-box img")
+        temperature (js/document.querySelector ".weather-box .temperature")
+        weather-details (js/document.querySelector ".weather-details")
+        weather-box (js/document.querySelector ".weather-box")
+        description (js/document.querySelector ".weather-box .description")
+        humidity (js/document.querySelector ".weather-details .humidity span")
+        wind (js/document.querySelector ".weather-details .wind span")
+        location-not-found (js/document.querySelector ".location-not-found")
+        weather (((json "weather") 0) "main")]
+
+    (.setProperty (. location-not-found -style) "display" "none")
+    (.remove (. location-not-found -classList) "fadeIn")
+
+    (cond
+      (= "Clear" weather) (.setAttribute image "src" "images/clear.png")
+      (= "Rain" weather) (.setAttribute image "src" "images/rain.png")
+      (= "Snow" weather) (.setAttribute image "src" "images/snow.png")
+      (= "Haze" weather) (.setAttribute image "src" "images/haze.png")
+      (= "Mist" weather) (.setAttribute image "src" "images/mist.png")
+      (= "Clouds" weather) (.setAttribute image "src" "images/cloud.png")
+      :else (.setAttribute image "src" ""))
+
+    (set! (.-innerHTML temperature) ((json "main") "temp"))
+    (set! (.-innerHTML description) (((json "weather") 0) "description"))
+    (set! (.-innerHTML humidity) ((json "main") "humidity"))
+    (set! (.-innerHTML wind) ((json "wind") "speed"))
+
+    (.setProperty (. weather-box -style) "display" "")
+    (.setProperty (. weather-details -style) "display" "")
+    (.add (. weather-box -classList) "fadeIn")
+    (.add (. weather-details -classList) "fadeIn")
+    (.setProperty (. container -style) "height" "590px")))
+
+(defn search-weather []
+  (let [city (.-value (js/document.querySelector ".search-box input"))]
+    (if (empty? city)
+      '()
+      (-> (request-api city)
+          (.then (fn [response]
+                   (-> (js/JSON.stringify response)
+                       js/JSON.parse
+                       js->clj)))
+          (.then (fn [json]
+                   (if (= "404" (json "cod"))
+                     (error-location-not-found)
+                     (set-weather-info json))))))))
 
 (defnc root-view []
   (d/div {:class "container"}
@@ -14,7 +83,8 @@
                 (d/i {:class "fa-solid fa-location-dot"})
                 (d/input {:type "text"
                           :placeholder "Enter your location"})
-                (d/button {:class "fa-solid fa-magnifying-glass"}))
+                (d/button {:class "fa-solid fa-magnifying-glass"
+                           :on-click search-weather}))
 
          (d/div {:class "location-not-found"}
                 (d/img {:src "images/location-not-found.png"})
